ffd2fde2dbdac0bf6c938c0b6e65a5aa5454fa29
George Olson <georgeolson92@gmail.com>
2016-09-16 08:25:06 -0700

    Add tests to project

---
+++ NETEventManager/EventManager.Tests/ModelTests/EventTest.cs
@@ -0,0 +1,22 @@
+﻿using EventManager.Models;
+using Xunit;
+
+namespace EventManager.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var item = new Event();
+
+            //Act
+            item.Description = "Wash the dog";
+            var result = item.Description;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
+    }
+}
