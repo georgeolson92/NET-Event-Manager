c6cff56df2c26d5ac8fe732fe0d751aefaf42ba8
George Olson <georgeolson92@gmail.com>
2016-09-16 11:09:41 -0700

    Add test project, implement Model tests and successfully pass, begin implementation of Controller tests

---
+++ NETEventManager/EventManager.Tests/ModelTests/EventTest.cs
@@ -0,0 +1,22 @@
+﻿using EventManager.Models;
+using Xunit;
+
+namespace EventManager.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetTitleTest()
+        {
+            //Arrange
+            var item = new Event();
+            item.Title = "A band playing a show";
+
+            //Act
+            var result = item.Title;
+
+            //Assert
+            Assert.Equal("A band playing a show", result);
+        }
+    }
+}
---
+++ NETEventManager/EventManager.Tests/ModelTests/VenueTest.cs
@@ -0,0 +1,22 @@
+﻿using EventManager.Models;
+using Xunit;
+
+namespace EventManager.Tests
+{
+    public class VenueTest
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            //Arrange
+            var item = new Venue();
+            item.Name = "Roseland Theater";
+
+            //Act
+            var result = item.Name;
+
+            //Assert
+            Assert.Equal("Roseland Theater", result);
+        }
+    }
+}
